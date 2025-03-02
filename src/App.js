00f57d3ba967d577347897659b82a4e1cb418b0f
TalbyWhistler <000748591@mohawkcollege.ca>
2025-03-02 14:50:48 -0500

    Functioning button that changes the state of the card grid

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 
@@ -10,9 +12,98 @@ function App() {
     <div className="App">
      
       {Counter()}
+      {GenerateButton()}
+      <div>
+          {CardGridB(deckCards,12)}
+      </div>
     </div>
   );
 }
+
+
+let deckCards = [];
+let displayCards = [];
+for (let i=0;i<4;i++)
+{
+    for (let j=1;j<=13;j++)
+    {
+        deckCards.push(<Card suit={i} value={j} key={`${i}-${j}`}/>);
+    }
+}
+
+function moveDeckCardsToDisplayed(deckCards,displayCards)
+{
+    displayCards.push(deckCards.pop());
+}
+
+function moveDisplayCardsToDeckCards(deckCards,displayCards)
+{
+    deckCards.push(displayCards.pop());
+}
+
+
+function CardGrid(deckCards)
+{
+    let rows = [];
+    let columns = [];
+    let cardCounter = 0;
+    while ( cardCounter <= 52)
+    {
+      for (let h=0;h<=6;h++)
+      {
+        for (let i=0;i<=10;i++)
+          {
+              let selectedCard=deckCards[cardCounter];
+              columns.push(<div className="col">{selectedCard}</div>)
+              cardCounter++;   
+          }
+          rows.push(<div className="row">{columns}</div>);
+          columns = [];
+      }
+        
+    }
+    return <div>{rows}</div>
+}
+
+function CardGridB(deckCards,onTheTable)
+{
+    console.log("On the table is " + onTheTable);
+    const [displayed,setDisplay] = useState(onTheTable);
+    let rows = [];
+    let columns = [];
+    let cardCounter = 0;
+
+    function incrementCardGrid()
+    {
+        setDisplay(displayed+1);
+    }
+    
+    while ( cardCounter <= displayed && cardCounter <= 52)
+    {
+      for (let h=0;h<=6;h++)
+      {
+        for (let i=0;i<=10;i++)
+          {
+            if ( cardCounter < displayed )
+            {
+              let selectedCard=deckCards[cardCounter];
+              columns.push(<div className="col">{selectedCard}</div>)
+              cardCounter++;   
+            }
+            else
+            {
+              cardCounter++;
+            }            
+          }
+          rows.push(<div className="row">{columns}</div>);
+          columns = [];
+      }
+        
+    }
+    return <div><button onClick={incrementCardGrid}>Test Increment Card Grid</button>{rows}</div>;
+}
+
+
 
 function Rectangle({propText}) {
   const style = {
@@ -56,6 +147,20 @@ function Counter() {
   return (
       <button onClick={incrementCount}>Count: {count}</button>
   );
+}
+
+
+function test0()
+{
+    return <p>Testo</p>
+}
+
+function GenerateButton()
+{
+    
+    return(
+      <button onClick={moveDisplayCardsToDeckCards(deckCards,displayCards)}>moveDisplayCardsToDeckCards()</button>
+    );
 }
 
 export default App;
